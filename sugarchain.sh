95339df4baafb4b4384f5898da757b3837d30031
yogeshme1994 <75430476+yogeshme1994@users.noreply.github.com>
2021-12-09 19:55:41 +0530

    Create sugarchain.sh

---
+++ sugarchain.sh
@@ -0,0 +1,5 @@
+#sugarchain
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qza3xdraqcrh4ussrax9ujcxh6mtn2568cwge8r.Yogesh12
